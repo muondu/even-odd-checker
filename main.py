ce860d21a4c88d1ea56913c0f66c9065e17db4e4
moundu <munenemuondu@gmail.com>
2020-08-19 10:07:01 +0300

    I have made some changes

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 import time
-input1 = int(input("Enter a number: "))
-def even_odd(number = input1):
+# input1 = int(input("Enter a number: "))
+# def even_odd(number = input1):
+#     """Dock strings"""
+#     if input1 % 2 == 0:
+#          even = print("The number is even")
+#          return even
+#     else:
+#         odd = print("The number is odd")
+#         return odd
+# print(even_odd())
+
+def take_input():
+    """Input taken"""
+    global input1
+    input1 = int(input("Enter a number: "))
+    return input1
+take_input()
+def is_even():
     if input1 % 2 == 0:
          even = print("The number is even")
          return even
     else:
         odd = print("The number is odd")
         return odd
-print(even_odd())
+is_even()
